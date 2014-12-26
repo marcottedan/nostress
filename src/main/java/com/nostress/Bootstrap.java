3b9f01d04daa4592537633070213b7c12d764cf9
Daniel Marcotte <marcottedan@gmail.com>
2014-12-25 21:12:58 -0500

    Added Logback, Redisson and Javassist. Bootstrap works fine with RedisClientStoreFactory

--- src/main/java/com/nostress/Bootstrap.java
+++ src/main/java/com/nostress/Bootstrap.java
@@ -1,7 +1,38 @@
 package com.nostress;
 
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.store.RedissonStoreFactory;
+import com.corundumstudio.socketio.store.StoreFactory;
+import org.redisson.Config;
+import org.redisson.Redisson;
+
 /**
  * Created by Daniel on 2014-12-25.
  */
 public class Bootstrap {
+
+    public static void main (String[] args){
+        // Instantiate Redisson configuration
+        Config redissonConfig = new Config();
+        redissonConfig.useSingleServer().setAddress("127.0.0.1:6379");
+
+        // Instantiate Redisson connection
+        Redisson redisson = Redisson.create(redissonConfig);
+
+        // Instantiate RedissonClientStoreFactory
+        RedissonStoreFactory redisStoreFactory = new RedissonStoreFactory();
+
+        // Instantiate SocketIO Configuration
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(8080);
+        config.setStoreFactory(redisStoreFactory);
+
+        // Instantiate SocketIO Server
+        SocketIOServer server = new SocketIOServer(config);
+
+        // Start the SocketIO Server
+        server.start();
+    }
 }
